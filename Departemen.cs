06f13102c0d796afdfdaad19fd247b172afbfd28
Nur Rochman <114822328+nurrochman954@users.noreply.github.com>
2024-12-02 09:47:25 +0700

    Add files via upload

--- Departemen.cs
+++ Departemen.cs
@@ -8,5 +8,17 @@ namespace Responsi2Junpro
 {
     internal class Departemen
     {
+        private int id_dep;
+        private string nama_dep;
+
+        public int ID_Dep 
+        {
+            get { return  id_dep; }
+        }
+        public string Nama_Dep 
+        {
+            get { return nama_dep; }
+            set { nama_dep = value; }
+        }
     }
 }
--- Karyawan.cs
+++ Karyawan.cs
@@ -8,5 +8,29 @@ namespace Responsi2Junpro
 {
     internal class Karyawan
     {
+        private string id_karyawan;
+        private string nama;
+        private int id_dep;
+        private string nama_dep;
+
+        public string ID_Karyawan
+        {
+            get { return id_karyawan; }
+            set { id_karyawan = value; }
+        }
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public int ID_Dep
+        {
+            get { return id_dep; }
+        }
+        public string Nama_Dep
+        {
+            get { return nama_dep; }
+            set { nama_dep = value; }
+        }
     }
 }
